87d3f9a6833f87a979f1355cc68fe3f3ba60c02d
Vadim <im@demetrodon.com>
2014-03-07 09:40:46 +0200

    test commit

---
+++ my.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @link http://web-systems.com.ua/
+ * @link http://price-r.ru/
+ * @copyright Copyright (c) 2013 - 2014 price-R.ru
+ * @license http://opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
+ */
+
+echo "I work";
+ 
